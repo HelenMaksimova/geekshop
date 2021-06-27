44e266add41a5df920f13546488f3d709dd63925
Elena <alnorda@gmail.com>
2021-06-28 00:27:49 +0300

    Implemented dynamic loading of content (from a json file)

--- products/views.py
+++ products/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from json import loads
+import pathlib
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = from_json_load('products.json')
+    return render(request, 'products/products.html', context)
+
+
+def from_json_load(filename):
+    filepath = pathlib.Path('products/fixtures/').joinpath(filename)
+    try:
+        data = loads(filepath.open(encoding='utf8').read())
+        return data
+    except IOError as error:
+        print(error)
+
