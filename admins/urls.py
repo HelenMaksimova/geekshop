93dc9eb9e6825c202a8c72d5365b49e52425036f
helen <alnorda@gmail.com>
2021-07-13 21:27:56 +0300

    created controllers for read and delete users (admins app), renewed template and urls

--- admins/urls.py
+++ admins/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('users/read/', admin_users_read, name='users_read'),
     path('users/create/', admin_users_create, name='users_create'),
-    path('users/update/', admin_users_update, name='users_update'),
-    path('users/delete/', admin_users_delete, name='users_delete'),
+    path('users/update/<int:id>/', admin_users_update, name='users_update'),
+    path('users/delete/<int:id>/', admin_users_delete, name='users_delete'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from users.models import User
 
 
 def index(request):
@@ -6,16 +7,22 @@ def index(request):
 
 
 def admin_users_read(request):
-    return render(request, 'admins/admin-users-read.html')
+    context = {
+        'title': 'Административная панель - Пользователи',
+        'admin_users': User.objects.all(),
+    }
+    return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html')
 
 
-def admin_users_update(request):
+def admin_users_update(request, id):
     return render(request, 'admins/admin-users-update-delete.html')
 
 
-def admin_users_delete(request):
-    pass
+def admin_users_delete(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = False
+    user.save()
