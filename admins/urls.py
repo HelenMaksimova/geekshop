40296d4e93e52cbb763a3644856c602c1168b66a
Elena <alnorda@gmail.com>
2021-07-18 22:34:17 +0300

    refactored admins app's views with using CVB

--- admins/urls.py
+++ admins/urls.py
@@ -1,28 +1,25 @@
 from django.urls import path
 from admins.views import (
     index,
-    admin_users_read, admin_users_create,
-    admin_users_update, admin_users_delete,
-    admin_categories_read, admin_categories_create,
-    admin_categories_update, admin_categories_delete,
-    admin_goods_read, admin_goods_create,
-    admin_goods_update, admin_goods_delete,
+    AdminUserListView, AdminUserCreateView, AdminUserUpdateView, AdminUserDeleteView,
+    AdminCategoryListView, AdminCategoryCreateView, AdminCategoryUpdateView, AdminCategoryDeleteView,
+    AdminGoodListView, AdminGoodCreateView, AdminGoodUpdateView, AdminGoodDeleteView,
 )
 
 app_name = 'admins'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('users/read/', admin_users_read, name='users_read'),
-    path('users/create/', admin_users_create, name='users_create'),
-    path('users/update/<int:id>/', admin_users_update, name='users_update'),
-    path('users/delete/<int:id>/', admin_users_delete, name='users_delete'),
-    path('categories/read/', admin_categories_read, name='categories_read'),
-    path('categories/create/', admin_categories_create, name='categories_create'),
-    path('categories/update/<int:id>/', admin_categories_update, name='categories_update'),
-    path('categories/delete/<int:id>/', admin_categories_delete, name='categories_delete'),
-    path('goods/read/', admin_goods_read, name='goods_read'),
-    path('goods/create/', admin_goods_create, name='goods_create'),
-    path('goods/update/<int:id>/', admin_goods_update, name='goods_update'),
-    path('goods/delete/<int:id>/', admin_goods_delete, name='goods_delete'),
+    path('users/read/', AdminUserListView.as_view(), name='users_read'),
+    path('users/create/', AdminUserCreateView.as_view(), name='users_create'),
+    path('users/update/<int:pk>/', AdminUserUpdateView.as_view(), name='users_update'),
+    path('users/delete/<int:pk>/', AdminUserDeleteView.as_view(), name='users_delete'),
+    path('categories/read/', AdminCategoryListView.as_view(), name='categories_read'),
+    path('categories/create/', AdminCategoryCreateView.as_view(), name='categories_create'),
+    path('categories/update/<int:pk>/', AdminCategoryUpdateView.as_view(), name='categories_update'),
+    path('categories/delete/<int:pk>/', AdminCategoryDeleteView.as_view(), name='categories_delete'),
+    path('goods/read/', AdminGoodListView.as_view(), name='goods_read'),
+    path('goods/create/', AdminGoodCreateView.as_view(), name='goods_create'),
+    path('goods/update/<int:pk>/', AdminGoodUpdateView.as_view(), name='goods_update'),
+    path('goods/delete/<int:pk>/', AdminGoodDeleteView.as_view(), name='goods_delete'),
 ]
