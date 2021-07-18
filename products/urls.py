a7bd59eecee35352a9f970327f09eee65e9df432
Elena <alnorda@gmail.com>
2021-07-19 01:05:45 +0300

    refactored products app's views with using CVB

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from products.views import products
+from products.views import ProductListView
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('<int:category_id>/', products, name='product'),
-    path('page/<int:page>/', products, name='page'),
+    path('', ProductListView.as_view(), name='index'),
+    path('<int:category_id>/', ProductListView.as_view(), name='product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from products.models import Product, ProductCategory
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic.list import ListView
 
 
 def index(request):
@@ -10,18 +11,21 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None, page=1):
-    products_list = Product.objects.all() if not category_id else Product.objects.filter(category_id=category_id)
-    paginator = Paginator(products_list, 3)
-    try:
-        products_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
-    context = {
-        'title': 'GeekShop - Каталог',
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all()
-    }
-    return render(request, 'products/products.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        return Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context.update(
+            {
+                'title': 'GeekShop - Каталог',
+                'categories': ProductCategory.objects.all(),
+            }
+        )
+        return context
