3589bdd8bbe474c7087b196acde4fc0e06478a71
helen <alnorda@gmail.com>
2021-08-23 22:19:40 +0300

    refactored using memcached

--- products/views.py
+++ products/views.py
@@ -1,6 +1,44 @@
 from products.models import Product, ProductCategory
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.core.cache import cache
+
+
+def get_categories_all():
+    if settings.LOW_CACHE:
+        key = 'categories_all'
+        categories_all = cache.get(key)
+        if categories_all is None:
+            categories_all = ProductCategory.objects.all()
+            cache.set(key, categories_all)
+        return categories_all
+    else:
+        return ProductCategory.objects.all()
+
+
+def get_products_all():
+    if settings.LOW_CACHE:
+        key = 'products_all'
+        products_all = cache.get(key)
+        if products_all is None:
+            products_all = Product.objects.all().select_related('category').order_by('pk')
+            cache.set(key, products_all)
+        return products_all
+    else:
+        return Product.objects.all().select_related('category').order_by('pk')
+
+
+def get_products_in_category(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_{pk}'
+        products_in_category = cache.get(key)
+        if products_in_category is None:
+            products_in_category = Product.objects.filter(category__pk=pk).order_by('pk')
+            cache.set(key, products_in_category)
+        return products_in_category
+    else:
+        return Product.objects.filter(category__pk=pk).order_by('pk')
 
 
 class IndexTemplateView(TemplateView):
@@ -14,10 +52,11 @@ class ProductListView(ListView):
     paginate_by = 3
     extra_context = {
         'title': 'GeekShop - Каталог',
-        'categories': ProductCategory.objects.all().select_related(),
+        'categories': get_categories_all()
     }
 
     def get_queryset(self):
         category_id = self.kwargs.get('category_id')
-        return Product.objects.filter(category_id=category_id).select_related() \
-            if category_id else Product.objects.all().select_related()
+        return get_products_in_category(category_id) if category_id else get_products_all()
+
+
